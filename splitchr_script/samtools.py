22148e9558d7e9ed451539979b2856f61b01d7e6
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-01 15:32:19 +0800

    Delete samtools.py

--- splitchr_script/samtools.py
+++
@@ -1,6 +0,0 @@
-f = open("Galili.Chr.fasta.fai","r")
-for line in f :
-    contig = line.strip().split()[0]
-    contig2 = line.strip().split()[0]
-    print("samtools faidx Galili.Chr.fasta %s > %s" %(contig,contig2))
-
