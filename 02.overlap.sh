26fadd87e203a38e87e7475637472cedb72652ad
古郡 まさき <108216941+gotouerina@users.noreply.github.com>
2023-06-01 16:18:10 +0800

    Update 02.overlap.sh

--- 02.overlap.sh
+++ 02.overlap.sh
@@ -1,5 +1,6 @@
 ##需要修改GFF文件路径
 gffpath=/path/gff
-do	
-	perl $t/CNV_gene-overlap.pl $gffpath *CNVs ${dic##*/}  \> output_file  >> $t/step3plus.sh
+for i in CNV/*CNVs
+do
+perl CNV_gene-overlap.pl $gffpath $i  > $i.overlap
 done
